37f87f2d895050116d56966d8522a7357dfe3085
Jacob Vanderlaan <jvander@utah.gov>
2022-01-03 10:17:39 -0700

    Updating for GSL summit

---
+++ dashboard/draft_components.R
@@ -0,0 +1,39 @@
+
+# Gilbert Bay salt pools
+Sidebar {.sidebar data-width=300}
+-------------------------------------
+[Details and source code for these estimates](https://bookdown.org/jakevl/gsldb-analyses/){target="_blank"}
+
+Row
+-----------------------------------------------------------------------
+###
+```{r}
+salt_mass_plot
+```
+
+
+bibliography: C:/Users/jvander/Documents/master-bibliography/bibliography.bib
+link-citations: yes
+colorlinks: yes
+csl: freshwater-science.csl
+
+
+
+# Breach flows
+Sidebar {.sidebar data-width=300}
+-------------------------------------
+
+Row
+-----------------------------------------------------------------------
+### Breach flows
+```{r}
+breach_disch_ts
+```
+
+Row
+-----------------------------------------------------------------------
+### 
+```{r}
+breach_disch_hist
+```
+
